7d02661cf8a5cf0069de449b850817284d5193db
Ali19abbas <aliabbas110.aa36@gmail.com>
2024-02-23 07:27:00 +0530

    advance react 2

--- ExpenceTracker/Extracker1/src/Appdata.jsx
+++ ExpenceTracker/Extracker1/src/Appdata.jsx
@@ -1,6 +1,17 @@
+import React, {useState} from 'react';
 import './expences.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 export default function AppData(props){
+  const [title, setTitle]=useState(props.title)
+  const [amount, setAmount]=useState(props.amount)
+
+  function eventhandle(){
+    alert("button clicked")
+    setTitle("updated")
+    setAmount("$100")
+  }
     
       return (<div className="container cont">
         <div className="date">
@@ -9,8 +20,9 @@ export default function AppData(props){
             <h6>{props.time}</h6>
             
         </div>
-     <h3>{props.title}</h3>
-     <h3><span class="badge bg-secondary">{props.amount}</span></h3>
-     <button className='btn btn-danger' onClick={()=>{alert("button clicked")}}>Detete</button>
+
+     <h3>{title}</h3>
+     <h3><span class="badge bg-secondary">{amount}</span></h3>
+     <button className='btn btn-danger' onClick={eventhandle}>Detete</button>
 </div>)
 }
